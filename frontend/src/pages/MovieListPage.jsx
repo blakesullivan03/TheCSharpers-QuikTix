77400242b03bf2d00c5e140f2c49a87f9424b694
Blake S Turner <blakest@email.sc.edu>
2024-12-09 19:47:42 -0500

    Running only Backend in the Terminal

--- frontend/src/pages/MovieListPage.jsx
+++ frontend/src/pages/MovieListPage.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getMovies } from "../apiService";
+
 
 function MovieListPage() {
   const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
+
 
   useEffect(() => {
     getMovies()
       .then((movies) => {
-        //console.log("Fetched movies:", movies);
         setMovies(movies);
       })
       .catch((error) => {
-        //console.error("Error fetching movies:", error);
+        console.error("Error fetching movies:", error);
       });
   }, []);
 
@@ -22,18 +24,45 @@ function MovieListPage() {
       <h1>Available Movies</h1>
       <ul>
         {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link to={`/Movies/${movie.id}`}>
-            <div>
-              <h2>{movie.title}</h2>
-              <p>{movie.description}</p>
-            </div>
-            </Link>
+          <li key={movie.id} style={{ marginBottom: "20px" }}>
+              <div>
+                <h2 style={{ display: "inline-block", marginRight: "10px" }}>
+                  {movie.title}
+                </h2>
+                <button
+                  style={{
+                    marginLeft: "10px",
+                    padding: "5px 10px",
+                    backgroundColor: "#007bff",
+                    color: "white",
+                    border: "none",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => navigate(`/movies/${movie.id}`)}
+                >
+                  Buy Tickets
+                </button>
+                <button
+                  style={{
+                    marginLeft: "10px",
+                    padding: "5px 10px",
+                    backgroundColor: "#007bff",
+                    color: "white",
+                    border: "none",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => navigate(`/movies/${movie.id}/reviews`)}
+                >
+                  Add Review
+                </button>
+                <p>{movie.description}</p>
+              </div>
           </li>
         ))}
       </ul>
     </div>
   );
 }
+
 
 export default MovieListPage;
